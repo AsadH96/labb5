b54fd5c98461757a2c55a9d5c3225f68da484668
AsadH96 <asadhus@kth.se>
2016-10-19 16:22:58 +0200

    Add files via upload

--- Card.java
+++ Card.java
@@ -6,13 +6,13 @@
 package model;
 
 import javafx.animation.FadeTransition;
+import javafx.animation.SequentialTransition;
 import javafx.geometry.Pos;
-import static javafx.scene.layout.BorderPane.setAlignment;
-import javafx.scene.layout.GridPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.scene.text.Text;
 import javafx.util.Duration;
 
@@ -20,7 +20,7 @@ import javafx.util.Duration;
  *
  * @author Asad
  */
-public class Card extends StackPane {
+public final class Card extends StackPane {
 
     private final char content;
     private Rectangle rect;
@@ -29,16 +29,19 @@ public class Card extends StackPane {
 
     public Card(char character) {
         this.content = character;
+        
         rect = new Rectangle(70, 100);
         rect.setFill(Color.RED);
         rect.setStroke(Color.AQUA);
 
         text = new Text(String.valueOf(character));
-        text.setFont(Font.font(30));
+        text.setFont(Font.font(50));
         text.setStroke(Color.BLACK);
         setAlignment(Pos.CENTER);
 
         getChildren().addAll(rect, text);
+        
+        hideStart();
 
         hidden = true;
     }
@@ -51,34 +54,36 @@ public class Card extends StackPane {
         }
     }
 
-    /*public Rectangle getRectangle(){
-        return this.rect;
-    }*/
     public void show() {
-
-        //FadeTransition ft = new FadeTransition(Duration.seconds(1), text);
-        //ft.setToValue(1);
-        //ft.play();
+        FadeTransition ft = new FadeTransition(Duration.seconds(0.5), text);
+        ft.setToValue(1);
+//        ft.play();
+        SequentialTransition seqT=new SequentialTransition(text, ft);
+        seqT.play();
     }
 
-    public void hide( 
-    Card this) {
-        
-        //System.out.println(card.toString() + " kortet");
-        
-        //FadeTransition ft = new FadeTransition(Duration.seconds(1), this.text);
-        //ft.setToValue(0);
-        //ft.play();
-        //System.out.println("hejsan");
-        //hidden = true;
+    public void hide() {
+        FadeTransition ft = new FadeTransition(Duration.seconds(1), text);
+        ft.setToValue(0);
+//        ft.play(); 
+        SequentialTransition seqT=new SequentialTransition(text, ft);
+        seqT.play();
+    }
+    
+    public void hideStart() {
+        FadeTransition ft = new FadeTransition(Duration.seconds(0.0001), text);
+        ft.setToValue(0);
+        ft.play();        
     }
     
     public void setHiddenFalse(){
         hidden=false;
+        show();
     }
     
     public void setHiddenTrue(){
         hidden=true;
+        hide();
     }
 
     @Override
