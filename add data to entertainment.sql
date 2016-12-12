c072e4029088bf41e9a1a672bee96777084eb84a
Ulsaa360 <ulsbold@kth.se>
2016-12-12 21:25:42 +0100

    Add files via upload

---
+++ add data to entertainment.sql
@@ -0,0 +1,65 @@
+insert into Person
+values ('01','Micheal Jackson','Artist', 'USA');
+insert into Person
+values ('02', 'Justin Bieber', 'Artist', 'Canada');
+insert into Person
+values ('03', 'Selana Gomez', 'Artist', 'USA');
+insert into Person
+values ('04', 'Eminem', 'Artist', 'USA');
+insert into Person
+values ('05', 'Bruno Mars', 'Artist', 'USA');
+insert into Person
+values ('06', 'Zara Larsson', 'Artist', 'Sweden');
+insert into Person
+values ('07', 'Tupac', 'Artist', 'USA');
+insert into Person
+values ('08', 'Bruce Springsteen', 'Artist', 'USA');
+insert into Person
+values ('09', 'Bob Dylan', 'Artist', 'USA');
+insert into Person
+values ('10', 'Elvis Presley', 'Artist', 'USA');
+insert into Person
+values ('11', 'Madona', 'Artist', 'USA');
+insert into Person
+values ('12', 'Ariana Grande', 'Artist', 'USA');
+insert into Person
+values ('13', 'Adele', 'Artist', 'England');
+insert into Person
+values ('14', 'Chris Brown', 'Artist', 'USA');
+insert into Person
+values ('15', 'Lorde', 'Artist', 'New Zealand');
+insert into Person
+values ('16', 'Ed Sheeran', 'Artist', 'USA');
+
+
+insert into Genre
+values ('01', 'Pop');
+insert into Genre
+values ('02', 'R&B');
+insert into Genre
+values ('03', 'Hiphop');
+insert into Genre
+values ('04', 'Soul');
+insert into Genre
+values ('05', 'Rock');
+insert into Genre
+values ('06', 'House');
+insert into Genre
+values ('07', 'Tekno');
+
+insert into Album
+values ('01', 'Thriller', '01', '1982', '01');
+insert into Album
+values ('02', 'Uncover', '06', '2015', '01');
+insert into Album
+values ('03', 'Infinite', '04', '1982', '03');
+insert into Album
+values ('04', '25', '13', '2015', '01');
+insert into Album
+values ('05', 'Royalty', '14', '2015', '03');
+insert into Album
+values ('06', 'True blue', '11', '1986', '01');
+insert into Album
+values ('07', 'Loose change', '16', '2010', '01');
+insert into Album
+values ('08', 'The River', '08', '1980', '05');
